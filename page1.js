f48549ad8a363e2ba183c39cc107a11fd7213bac
Pallavi Pramod Kawade <121016747+pallavi-devop@users.noreply.github.com>
2023-09-06 09:40:01 +0530

    Create page1.js

---
+++ page1.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pallavi Kawade's Resume</title>
+    <!-- Add your CSS link here to style the resume -->
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+</head>
+<body>
+    <header>
+        <h1>Pallavi Kawade</h1>
+        <p>Email: <a href="mailto:pallavi.kawade123@gmail.com">pallavi.kawade123@gmail.com</a> | Contact Number: +917888142100</p>
+    </header>
+
+    <section>
+        <h2>Career Objective</h2>
+        <p>Red Hat Certified System Administrator with Cloud Engineer Intern experience in automating and optimizing software development processes. Skilled in cloud infrastructure management, containerization technologies, and CI/CD practices. Proficient in tools like AWS, Docker, Jenkins, Git, and Terraform. Passionate about driving efficiency, scalability, and collaboration. Strong problem-solving and communication skills. Committed to delivering high-quality, reliable, and secure solutions.</p>
+    </section>
+
+    <section>
+        <h2>Certification</h2>
+        <ul>
+            <li>Red Hat Certified System Administrator (RHCSA)</li>
+            <li>Certification Date: 22 March 2023</li>
+            <li>Technology Used: Red Hat Enterprise Linux 9</li>
+            <li>Certification ID: 230-046-831</li>
+            <li><a href="https://www.credly.com/badges/d5248fc7-f275-474b-8868-8364568f11a0/public_url">Credential URL</a></li>
+        </ul>
+    </section>
+
+</body>
+</html>
